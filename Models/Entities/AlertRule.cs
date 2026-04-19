61cd3c115fb132b5ed81cbf7a34b10b20ec00262
Raoof <ahmedraoof362005@gmail.com>
2026-04-20 00:18:05 +0200

    Rest of Entities

---
+++ Models/Entities/AlertRule.cs
@@ -0,0 +1,20 @@
+﻿using FinanceME.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace FinanceME.Models.Entities
+{
+    public class AlertRule
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; } = string.Empty;
+        public decimal Threshold { get; set; }
+        [Required]
+        public string UserId { get; set; } = string.Empty;
+        public virtual User? User { get; set; }
+        public virtual ICollection<Notification> GeneratedNotifications { get; set; } = new List<Notification>();
+
+    }
+}
---
+++ Models/Entities/Notification.cs
@@ -0,0 +1,21 @@
+﻿using FinanceME.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace FinanceME.Models.Entities
+{
+    public class Notification
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(200)]
+        public string Message { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+        public bool IsRead { get; set; }
+        [Required]
+        public string UserId { get; set; } = string.Empty;
+        public virtual User? User { get; set; }
+        public int? AlertRuleId {  get; set; }
+        public virtual AlertRule? AlertRule { get; set; }
+    }
+}
